061d1e35eec20836aec56b8de7ee2a212cd23e43
mayhemmmwith3ms <20123722@tafe.wa.edu.au>
2024-05-09 13:51:46 +0800

    Give Sad ability to blink

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ That is, delete the sense_hat.py file that is included in this bundle."""
 import time
 
 from happy import Happy
+from sad import Sad
 
 if __name__ == '__main__':
     # This is only needed if you have not deleted sense_hat.py
@@ -13,7 +14,7 @@ if __name__ == '__main__':
     ############################################################
 
     # Create a happy smiley, which is a subclass of Smiley
-    smiley = Happy()
+    smiley = Sad()
 
     # This is a form of #polymorphism, as the Happy smiley class
     # does not have a method called .show(). This means that
